d5a64de433d7e716142256fbd579558aad061a41
Christopher Perrin <cperrin88@gmail.com>
2025-09-25 15:59:02 +0200

    feat(artifact): add metadata parsing from file and stream
    - Added `ParseMetadataFromPath` to parse metadata from file paths.
    - Introduced `ParseMetadataFromStream` for metadata parsing from streams.

--- pkg/artifact/metadata.go
+++ pkg/artifact/metadata.go
@@ -1,6 +1,10 @@
 package artifact
 
 import (
+	"encoding/json"
+	"io"
+	"os"
+
 	"github.com/cperrin88/gotya/pkg/platform"
 	"github.com/hashicorp/go-version"
 )
@@ -38,4 +42,23 @@ func (m *Metadata) GetArch() string {
 		return platform.AnyArch
 	}
 	return m.Arch
+}
+
+func ParseMetadataFromPath(path string) (*Metadata, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return ParseMetadataFromStream(file)
+}
+
+func ParseMetadataFromStream(stream io.Reader) (*Metadata, error) {
+	var metadata Metadata
+	decoder := json.NewDecoder(stream)
+	if err := decoder.Decode(&metadata); err != nil {
+		return nil, err
+	}
+	return &metadata, nil
 }
