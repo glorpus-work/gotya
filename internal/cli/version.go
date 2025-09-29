2da1bba5dae6ac506231fd7a143312bff93abfbb
Christopher Perrin <cperrin88@gmail.com>
2025-09-29 14:13:22 +0200

    chore(cli/version.go): refactor version command to dynamically fetch build info

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,14 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
-)
-
-const (
-	Version   = "0.1.0"
-	BuildDate = "unknown"
-	GitCommit = "unknown"
 )
 
 // NewVersionCmd creates the version command.
@@ -25,7 +21,63 @@ func NewVersionCmd() *cobra.Command {
 }
 
 func runVersion(*cobra.Command, []string) {
-	fmt.Printf("gotya version %s\n", Version)
-	fmt.Printf("Build date: %s\n", BuildDate)
-	fmt.Printf("Git commit: %s\n", GitCommit)
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("Warning: Could not read build information")
+		return
+	}
+
+	version := getVersion(bi)
+	fmt.Printf("gotya version %s\n", version)
+	fmt.Printf("Build date: %s\n", getBuildDate(bi.Settings))
+	fmt.Printf("Git commit: %s\n", getGitCommit(bi.Settings))
+	fmt.Printf("Go version: %s\n", bi.GoVersion)
+}
+
+func getVersion(bi *debug.BuildInfo) string {
+	version := bi.Main.Version
+
+	// If version is empty or (devel), try to get from VCS
+	if version == "" || version == "(devel)" {
+		return getVersionFromVCS(bi.Settings)
+	}
+
+	// If version looks like a semantic version (contains dots), return as-is
+	if strings.Contains(version, ".") {
+		return version
+	}
+
+	// Otherwise, it's likely a commit hash, return the short version
+	return getVersionFromVCS(bi.Settings)
+}
+
+func getVersionFromVCS(settings []debug.BuildSetting) string {
+	for _, setting := range settings {
+		if setting.Key == "vcs.revision" {
+			commit := setting.Value
+			if len(commit) > 8 {
+				commit = commit[:8]
+			}
+			return commit
+		}
+	}
+	return "unknown"
+}
+
+func getBuildDate(settings []debug.BuildSetting) string {
+	for _, setting := range settings {
+		if setting.Key == "vcs.time" {
+			return setting.Value
+		}
+	}
+	return "unknown"
+}
+
+func getGitCommit(settings []debug.BuildSetting) string {
+	for _, setting := range settings {
+		if setting.Key == "vcs.revision" {
+			return setting.Value
+		}
+	}
+	return "unknown"
 }
